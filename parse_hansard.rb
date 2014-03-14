57168a0a10cbace346b871094a08d5eb479ecafb
Kyle Geske <stuneye@gmail.com>
2014-03-14 18:25:12 -0400

    Pretty good start on generating parsed output html.

--- parse_hansard.rb
+++ parse_hansard.rb
@@ -1,12 +1,20 @@
-# encoding: UTF-8
 require 'nokogiri'
 
-doc = Nokogiri::HTML(open('test_hansard.htm', "r:UTF-8"), nil, 'UTF-8')
+if ARGV.size != 2
+  puts "Missing required arguments!"
+  puts "Example: #{$0} input.html output.html"
+  exit
+end
+
+input_file = ARGV[0]
+output_file = ARGV[1]
+doc = Nokogiri::HTML(open(input_file, "r:UTF-8"), nil, 'UTF-8')
 
 paragraphs = doc.css('div > p')
 
-
 puts "Found #{paragraphs.size} paragraphs."
+
+attendees = ["Mayor Katz", "Councillor Sharma", "Councillor Browaty", "Councillor Eadie", "Councillor Fielding", "Councillor Gerbasi", "Councillor Havixbeck", "Councillor Mayes", "Councillor Nordman", "Councillor Orlikow", "Councillor Pagtakhan", "Councillor Smith", "Councillor Steen", "Councillor Swandel", "Councillor Vandal", "Councillor Wyatt"]
 
 new_capture = true
 captures = []
@@ -14,14 +22,8 @@ capture_index = -1
 
 speakers = Hash.new { |hash, key| hash[key] = [] }
 
-speaking = [ "Madam Speaker", "Mayor Katz", "Councillor Sharma", "Councillor Browaty", "Councillor Eadie", "Councillor Fielding", "Councillor Gerbasi", "Councillor Havixbeck", "Councillor Mayes", "Councillor Nordman", "Councillor Orlikow", "Councillor Pagtakhan", "Councillor Smith", "Councillor Steen", "Councillor Swandel", "Councillor Vandal", "Councillor Wyatt"]
-
 paragraphs.each do |p|
   children = p.children
-  #
-  #puts "Para has #{children.size} children: "
-  #children.each_with_index {|c, i| puts "Index #{i}"; puts c }
-  
   spans = p.css('span')
   
   new_capture = capture_index == -1 || (!spans.size.zero? && spans[0].content.include?(':'))
@@ -41,55 +43,60 @@ paragraphs.each do |p|
   end
   
   p.children.each do |child|
-    #puts child.content
-    captures[capture_index][:content] += child.content + " "   unless child.name == 'span' # This span rejection is too heavy a hammer. Strips out some required details.
-    #puts captures[capture_index][:content]
+    content = child.content
+    content.gsub!(/:/, "<span class='highlight1'>:</span>")  unless child.name == 'span'
+    content.gsub!(/Motion No/, "<span class='highlight2'>Motion No</span>")
+    content.gsub!(/([A-Z ]{6,})/, '<span class=\'highlight3\'>\1</span>')
+    captures[capture_index][:content] += content + " "
   end
 end
 
-speaking.each do |s|
-  word_count = 0
-  speakers[s].each do |words_index|
-    words = captures[words_index][:content]
-    words = words.sub("…"," ")
-    wc = words.split(' ')
-    word_count += wc.size
-  end
-  #
-  #puts "#{s} spoke #{speakers[s].count} times with #{word_count} words."
-  print "<div class='speaker_wc'>"
-  print "<img src='images/#{s.sub(' ','')}.jpg' title='#{s}' alt='#{s}'>"
-  print "<div class='wc' style='width: #{(word_count/4.0).round}px;'>#</div>"
-  print "<div class='words'>#{word_count} words<br>#{s}</div>"
-  puts "</div>"
+target = File.open(output_file, 'w:UTF-8')
+
+target.write("<!DOCTYPE html>
+<html>
+<head>
+  <title>Parsed Hansard - #{input_file}</title>
+  <meta charset='utf-8'>
+  <style>
+    span.highlight1 { background: #FAF741; padding: 0 2px; }
+    span.highlight2 { background: #A1FF96; }
+    span.highlight3 { background: #96FFC9; }
+  </style>
+</head>
+<body>
+<form>
+  <label>Date:</label>
+  <input type='text' name='date'>
+  <label>Time:</label>
+  <input type='text' name='time'>
+  <label>Prayer by:</label>
+  <input type='text' name='prayer_by'>
+  <label>Meeting Speaker:</label>
+  <input type='text' name='meeting_speaker'>
+  <br>")
+attendees.each do |attendee|
+  attendee_snake_case = attendee.gsub(' ','_')
+  target.write("<label for='#{attendee_snake_case}'>#{attendee}</label>")
+  target.write("<input type='checkbox' checked name='present' value='#{attendee}' id='#{attendee_snake_case}'>\n")
+end
+  
+target.write("</form>")
+
+captures.each do |c|
+  target.write("<div>\n")
+  target.write("<p>#{c[:content]}</p>\n")
+  target.write("<form>\n")
+  target.write("<label>Speaker:</label>")
+  target.write("<input type='text' name='speaker' value='#{c[:speaker]}'>\n")
+  target.write("<label>Spoken:</label>")
+  target.write("<input type='text' name='spoken'>\n")
+  target.write("<button class='mute'>mute</button>\n")
+  target.write("<button class=''>+ speaker</button>\n")
+  target.write("<button class='mute'>+ motion</button>\n")
+  target.write("</form>\n")
+  target.write("</div>\n")
 end
 
-#target = File.open('output.htm', 'w:UTF-8')
-#
-#target.write("<!DOCTYPE html>
-#<html>
-#<head>
-#  <title>Page Title</title>
-#  <meta charset='utf-8'>
-#</head>
-#<body>")
-#
-#captures.each do |c|
-#  target.write("<div class='speaker'>\n")
-#  target.write("<div class='image'>\n")
-#  if speaking.include?(c[:speaker])
-#    target.write("<img src='images/#{c[:speaker].sub(' ','')}.jpg'>")
-#  else
-#    target.write("<img src='images/missing.jpg'>")
-#  end
-#  target.write("<p>#{c[:speaker]}</p>\n")
-#  target.write("</div>\n")
-#  target.write("<p>#{c[:content]}</p>\n")
-#  puts c[:content]
-#  target.write("</p>")
-#  target.write("</div>")
-#end
-#
-#target.write('</body></html>')
-#
-#target.close
+target.write('</body></html>')
+target.close
