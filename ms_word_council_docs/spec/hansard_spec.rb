3a93c480011282f24119073c510be2877a112e70
Kyle Geske <stungeye@gmail.com>
2015-08-05 08:18:22 -0500

    Installed the docx gem and not a simple test up and running.

---
+++ ms_word_council_docs/spec/hansard_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Hansard do
+  it 'should be a sane world' do
+    expect(Hansard.new.class).to eq(Hansard)
+  end
+end
--- ms_word_council_docs/spec/spec_helper.rb
+++ ms_word_council_docs/spec/spec_helper.rb
@@ -1,4 +1,1 @@
-#require_relative '../file_helpers.rb'
-#require_relative '../hansard.rb'
-#require_relative '../erb_binding.rb'
-#require_relative '../pattern_counter.rb'
+require_relative '../hansard.rb'
