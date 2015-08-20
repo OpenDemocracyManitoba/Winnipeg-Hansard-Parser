8f1e59304e60ff23a22a998d8924db88af631624
Kyle Geske <stungeye@gmail.com>
2015-08-20 07:04:19 -0500

    Disposition spec is now testing a DOCX disposition.

--- ms_word_council_docs/disposition.rb
+++ ms_word_council_docs/disposition.rb
@@ -1,4 +1,10 @@
 require 'docx'
 
 class Disposition
+  def initialize(filename)
+    @doc = Docx::Document.open(filename)
+  end
+
+  private
+  attr_reader :doc
 end
--- ms_word_council_docs/spec/disposition_spec.rb
+++ ms_word_council_docs/spec/disposition_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Disposition do
   before(:all) do
-    @disposition = Disposition.new
+    @disposition = Disposition.new('Disposition-2015-03-25.docx')
   end
 
   it 'should be the correct type' do
