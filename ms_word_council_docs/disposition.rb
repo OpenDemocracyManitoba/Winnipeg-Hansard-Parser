9ffe5c19e766012bf0366c0829162e21a5345b8a
Kyle Geske <stungeye@gmail.com>
2015-08-22 07:55:59 -0500

    Correctly locate and parse bylaws from word document.

--- ms_word_council_docs/disposition.rb
+++ ms_word_council_docs/disposition.rb
@@ -8,8 +8,30 @@ class Disposition
     @doc = Docx::Document.open(filename)
   end
 
+  def bylaws
+    bylaw_data
+  end
+
   def_delegator :doc, :tables
 
   private
   attr_reader :doc
+
+  def bylaw_table
+    tables.select do |t| 
+      t.rows[0].cells[0].text == 'BY-LAWS PASSED (RECEIVED THIRD READING)'
+    end.first
+  end
+
+  def bylaw_table_rows
+    bylaw_table.rows[2..-1] # The first two rows are headers
+  end
+
+  def bylaw_data
+    bylaw_table_rows.map do |bylaw_row|
+      { number:      bylaw_row.cells[0].text,
+        subject:     bylaw_row.cells[1].text,
+        disposition: bylaw_row.cells[2].text == 'PASSED' }
+    end
+  end
 end
